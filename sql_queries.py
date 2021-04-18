59317125eb80dc5d70d0043425b129a42654d9b3
Wesley Bortolozo <wesleyjr0101@gmail.com>
2021-04-18 11:36:32 -0300

    changed data model of time table

--- sql_queries.py
+++ sql_queries.py
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS artists \
 
 time_table_create = """
 CREATE TABLE IF NOT EXISTS time \
-    (start_time TIMESTAMP, \
+    (start_time TIMESTAMP PRIMARY KEY, \
     hour INT, \
     day INT, \
     week INT, \
@@ -83,35 +83,46 @@ user_table_insert = """
 INSERT INTO users \
     (user_id, first_name, last_name, gender, level) \
     VALUES(%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO NOTHING
 """
 
 song_table_insert = """
 INSERT INTO songs \
     (song_id, title, artist_id, year, duration) \
-    VALUES(%s, %s, %s, %s, %s)
+    VALUES(%s, %s, %s, %s, %s) \
+    ON CONFLICT (song_id) DO NOTHING
 """
 
 artist_table_insert = """
 INSERT INTO artists \
     (artist_id, name, location, latitude, longitude) \
-    VALUES(%s, %s, %s, %s, %s)
+    VALUES(%s, %s, %s, %s, %s) \
+    ON CONFLICT (artist_id) DO NOTHING
 """
 
 
 time_table_insert = """
 INSERT INTO time \
     (start_time, hour, day, week, month, year, weekday) \
-    VALUES(%s, %s, %s, %s, %s, %s, %s)
+    VALUES(%s, %s, %s, %s, %s, %s, %s) \
+    ON CONFLICT (start_time) DO NOTHING
 """
 
 # FIND SONGS
+
+# song_select = """
+# SELECT songs.song_id, artists.artist_id FROM songs \
+#     JOIN artists ON artists.artist_id = songs.artist_id \
+#     WHERE songs.title LIKE %s \
+#     AND artists.name LIKE %s;
+# """
 
 song_select = """
 SELECT songs.song_id, artists.artist_id FROM songs \
     JOIN artists ON artists.artist_id = songs.artist_id \
     WHERE songs.title LIKE %s \
     AND artists.name LIKE %s \
-    AND songs.duration = %s;
+    AND songs.duration = CAST(%s AS INTEGER);
 """
 
 # QUERY LISTS
